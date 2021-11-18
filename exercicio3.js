29b5453dbe03bd9f378a2b2a2860d41b13bd6802
AllrightIsHere <gasilva912@gmail.com>
2021-11-18 15:03:31 -0300

    Exercício 3: expandir/retrair os parágrafos

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,12 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const btnsEl = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let btnEl of btnsEl) {
+    btnEl.addEventListener('click', (e) => {
+        const btnTargetEl = e.currentTarget;
+        const pEl = btnTargetEl.parentNode;
+        btnTargetEl.innerHTML = pEl.classList.toggle('expandido') ? '-' : '+';
+    });
+}
