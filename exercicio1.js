1354a0cac6914ae00632f7c717e011bbb42ac1fc
AllrightIsHere <gasilva912@gmail.com>
2021-11-18 14:18:58 -0300

    Exercício 1: cálculo da constante gravitacional

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,14 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const btnCalcularEl = document.querySelector('#calcular');
+
+btnCalcularEl.addEventListener('click', () => {
+    const constante = Number(document.querySelector('#constante').value);
+    const massa1 = Number(document.querySelector('#massa1').value);
+    const massa2 = Number(document.querySelector('#massa2').value);
+    const distancia = Number(document.querySelector('#distancia').value);
+
+    const resultEl = btnCalcularEl.previousElementSibling.firstElementChild;
+    resultEl.value = constante * massa1 * massa2 / (distancia ** 2);
+});
